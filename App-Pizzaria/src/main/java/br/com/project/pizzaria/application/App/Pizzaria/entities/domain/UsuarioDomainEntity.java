3de737b4ccc3e388d4a36b15c4f378015b4802d2
Erik Santos <erik311009r@gmail.com>
2021-08-27 21:51:10 -0300

    add(usuario): add atributo usuarioDomain
    Adicionei nome para o usuario
    
    refs:[PP-13]

--- App-Pizzaria/src/main/java/br/com/project/pizzaria/application/App/Pizzaria/entities/domain/UsuarioDomainEntity.java
+++ App-Pizzaria/src/main/java/br/com/project/pizzaria/application/App/Pizzaria/entities/domain/UsuarioDomainEntity.java
@@ -1,4 +1,11 @@
 package br.com.project.pizzaria.application.App.Pizzaria.entities.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UsuarioDomainEntity {
+
+    private String nome;
 }
