6b593c87714763366ae4064289518aeee7829e1b
Erik Santos <erik311009r@gmail.com>
2021-09-02 09:35:59 -0300

    feat(endpoint): add api user
    Adicionando e implementando a API de users
    
    Refs: [PP-14]

--- App-Pizzaria/src/main/java/com/system/application/pizzaria/endpoint/UserController.java
+++ App-Pizzaria/src/main/java/com/system/application/pizzaria/endpoint/UserController.java
@@ -1,10 +1,24 @@
 package com.system.application.pizzaria.endpoint;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.*;
+
 @RestController
-@RequestMapping(value = "v1/user", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/v1/user", produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
+
+    @GetMapping
+    public Map<String, String> getListaUsuarios(){
+        Map<String, String> listaAlunos = new HashMap<>();
+        listaAlunos.put("Nome1:", "Erik");
+        listaAlunos.put("Nome2:", "Luan");
+        listaAlunos.put("Nome3", "Sarina");
+        listaAlunos.put("Nome4", "Marcos");
+        listaAlunos.put("Nome5", "Marcio");
+        return listaAlunos;
+    }
 }
