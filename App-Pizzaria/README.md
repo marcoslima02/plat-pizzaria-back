2d97977dbb970558ac8b3a2111d98fb366acc5d1
Erik Santos <erik311009r@gmail.com>
2021-09-01 21:35:25 -0300

    Inicio projeto

--- App-Pizzaria/README.md
+++ App-Pizzaria/README.md
@@ -1,3 +1,10 @@
-## INSTRUCOES DOCKER
+## Instruções  para rodar applicação
+1: Abra o docker
 
-1: docker-compose -f docker-compose.yml up
+# Digite no terminal
+2: mvn clean install
+
+# Digite no terminal
+3: docker-compose -f docker-compose.yml up
+
+4: start a applicação
--- App-Pizzaria/src/main/java/com/system/application/pizzaria/external/database/entity/UserModel.java
+++ App-Pizzaria/src/main/java/com/system/application/pizzaria/external/database/entity/UserModel.java
@@ -1,4 +1,17 @@
 package com.system.application.pizzaria.external.database.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    private long id;
 }
