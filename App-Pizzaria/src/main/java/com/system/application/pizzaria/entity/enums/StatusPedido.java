35bfc3ee02b0351876d34eb7ec205805bd698fc6
Erik Santos <eriksantos@ciandt.com>
2021-09-03 18:04:59 -0300

    feat(status_pedido): finished status pedido
    finalização do Enum StatusPedido.
    
    refs:[PP-22]

---
+++ App-Pizzaria/src/main/java/com/system/application/pizzaria/entity/enums/StatusPedido.java
@@ -0,0 +1,23 @@
+package com.system.application.pizzaria.entity.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum StatusPedido {
+
+    PENDENTE(1,"Pedido não finalizado corretamente"),
+    AGUARDANDO(2,"Pedido recebido, mas não pago ainda e não iniciado pelo atendente"),
+    PROCESSANDO(3,"Pedido recebido, iniciado pelo atendente mas não pago ainda"),
+    FINALIZADO(4,"Pedido Finalizado, porém não enviado para o Cliente"),
+    CONCLUIDO(5,"Pedido Concluido, produto entregue ao cliente"),
+    CANCELADO(6,"Pedido Cancelado, caso o pagamento não ocorra"),
+    REEMBOLSADO(7,"Pedido Reembolsado, valor devolvido ao comprador");
+
+    private final String descricao;
+    private final Integer code;
+
+    StatusPedido(int code, String descricao) {
+        this.code = code;
+        this.descricao = descricao;
+    }
+}
