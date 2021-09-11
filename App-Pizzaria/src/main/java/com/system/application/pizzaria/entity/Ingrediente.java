1bc4ee77b34c19e2b1103cbd923ffdc9454bdafc
Marcio Santos <marcio.gabriel2cs@gmail.com>
2021-09-11 18:19:07 -0300

    feat(Ingrediente): add class endereco
    Adicionando arquivos para criação da classe Ingrediente
    
    refs: PP-22

--- App-Pizzaria/src/main/java/com/system/application/pizzaria/entity/Ingrediente.java
+++ App-Pizzaria/src/main/java/com/system/application/pizzaria/entity/Ingrediente.java
@@ -1,4 +1,23 @@
 package com.system.application.pizzaria.entity;
 
-public class Ingrediente {
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+public abstract class Ingrediente {
+
+    private Integer id;
+
+    @Setter
+    private String nome;
+
+    @Setter
+    private Integer quantidade;
+
+    @Setter
+    private BigDecimal preco;
+
+
 }
--- App-Pizzaria/src/main/java/com/system/application/pizzaria/viewmodel/IngredienteVM.java
+++ App-Pizzaria/src/main/java/com/system/application/pizzaria/viewmodel/IngredienteVM.java
@@ -1,4 +1,21 @@
 package com.system.application.pizzaria.viewmodel;
 
-public class IngredienteVM {
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+public abstract class IngredienteVM {
+
+    private Integer id;
+
+    @Setter
+    private String nome;
+
+    @Setter
+    private Integer quantidade;
+
+    @Setter
+    private BigDecimal preco;
 }
