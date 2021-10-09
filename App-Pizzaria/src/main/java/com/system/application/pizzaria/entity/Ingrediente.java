c11a6d497744598c87565f2d6242d3d533c6cba5
marcoslima02 <marcos.lima02@gmail.com>
2021-10-09 00:20:32 -0300

    feat(ingrediente): Finish ingrediente adapters and exceptions
    Finalizado
    
    refs: PP-37

--- App-Pizzaria/src/main/java/com/system/application/pizzaria/entity/Ingrediente.java
+++ App-Pizzaria/src/main/java/com/system/application/pizzaria/entity/Ingrediente.java
@@ -1,5 +1,6 @@
 package com.system.application.pizzaria.entity;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import java.math.BigDecimal;
 
 @Getter
 @Setter
+@EqualsAndHashCode
 public class Ingrediente {
 
     private Integer idIngrediente;
