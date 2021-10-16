b12ccad06c900a40839c08f43d16370be265be3e
Erik Santos <erik311009r@gmail.com>
2021-10-15 22:18:45 -0300

    subindo config

---
+++ App-Pizzaria/src/main/java/com/system/application/pizzaria/util/Configuration.java
@@ -0,0 +1,20 @@
+package com.system.application.pizzaria.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
