0e2add2afdf7c2f0867f0244b636d617e5df3e78
marcoslima02 <marcos.lima02@gmail.com>
2021-10-18 17:05:43 -0300

    feat(adm): finish in Cozinheiro tests
    Finalizado os testes dos adapters de Cozinheiro
    
    refs: PP-34

--- App-Pizzaria/src/test/java/com/system/application/pizzaria/model/CozinheiroModelAdapterTest.java
+++ App-Pizzaria/src/test/java/com/system/application/pizzaria/model/CozinheiroModelAdapterTest.java
@@ -1,12 +1,16 @@
 package com.system.application.pizzaria.model;
 
 import com.system.application.pizzaria.entity.*;
+import com.system.application.pizzaria.entity.enums.CategoriaPizza;
+import com.system.application.pizzaria.entity.enums.StatusPedido;
 import com.system.application.pizzaria.exception.CozinheiroException;
 import com.system.application.pizzaria.external.database.entity.*;
 import com.system.application.pizzaria.external.database.entity.adapter.CozinheiroModelAdapter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -62,7 +66,45 @@ public class CozinheiroModelAdapterTest {
         Cozinheiro cozinheiro = new Cozinheiro();
         List<Pedido> listPedido = new ArrayList<>();
         List<Ingrediente> listIngrediente = new ArrayList<>();
+        cozinheiro.setIdCozinheiro(1);
+        cozinheiro.setListaPizzaPedidoCozinheiro(listPedido);
+        cozinheiro.setListaIngredientesPizzaCozinheiro(listIngrediente);
 
+        Pedido pedido = new Pedido();
+        List<Pizza> listPizza = new ArrayList<>();
+        List<Bebida> listBebida = new ArrayList<>();
+        pedido.setIdPedido(1);
+        pedido.setStatusPedido(StatusPedido.AGUARDANDO);
+        pedido.setListaPizzaPedido(listPizza);
+        pedido.setListaBebidaPedido(listBebida);
+        pedido.setHorarioPedido(LocalDateTime.now());
+        pedido.setHorarioEstimadoPedido(LocalDateTime.now());
+        pedido.setPrecoPedido(new BigDecimal("59.00"));
+        pedido.setComentarioPedido("fnfsjngdsnofgjlig, dhufsufhsfgbhsbgf fdhiuashfihasidf.");
+
+        Pizza pizza = new Pizza();
+        pizza.setIdPizza(1);
+        pizza.setPrecoPizza(new BigDecimal("59.00"));
+        //pizza.setListaIngredientesPizza();
+        pizza.setCategoriaPizza(CategoriaPizza.LIGHT);
+
+        Bebida bebida = new Bebida();
+        bebida.setIdBebida(1);
+        bebida.setNomeBebida("Sua mãe");
+        bebida.setQuantidadeBebida(17);
+        bebida.setPrecoBebida(new BigDecimal("59.00"));
+
+        listPizza.add(pizza);
+        listBebida.add(bebida);
+
+        Ingrediente ingrediente = new Ingrediente();
+        ingrediente.setIdIngrediente(1);
+        ingrediente.setNomeIngrediente("Calabresa");
+        ingrediente.setQuantidadeIngrediente(3);
+        ingrediente.setPrecoIngrediente(new BigDecimal(3.99));
+
+        listPedido.add(pedido);
+        listIngrediente.add(ingrediente);
         return cozinheiro;
     }
 
@@ -70,7 +112,45 @@ public class CozinheiroModelAdapterTest {
         CozinheiroModel cozinheiroModel = new CozinheiroModel();
         List<PedidoModel> listPedidoModel = new ArrayList<>();
         List<IngredienteModel> listIngredienteModel = new ArrayList<>();
+        cozinheiroModel.setIdCozinheiroModel(1);
+        cozinheiroModel.setListaPizzaPedidoModelCozinheiroModel(listPedidoModel);
+        cozinheiroModel.setListaIngredientesPizzaModelCozinheiroModel(listIngredienteModel);
 
+        PedidoModel pedidoModel = new PedidoModel();
+        List<PizzaModel> listPizzaModel = new ArrayList<>();
+        List<BebidaModel> listBebidaModel = new ArrayList<>();
+        pedidoModel.setIdPedidoModel(1);
+        pedidoModel.setStatusPedidoModel(StatusPedido.AGUARDANDO);
+        pedidoModel.setListaPizzaModelPedidoModel(listPizzaModel);
+        pedidoModel.setListaBebidaModelPedidoModel(listBebidaModel);
+        pedidoModel.setHorarioPedidoModel(LocalDateTime.now());
+        pedidoModel.setHorarioEstimadoPedidoModel(LocalDateTime.now());
+        pedidoModel.setPrecoPedidoModel(new BigDecimal("59.00"));
+        pedidoModel.setComentarioPedidoModel("fnfsjngdsnofgjlig, dhufsufhsfgbhsbgf fdhiuashfihasidf.");
+
+        PizzaModel pizzaModel = new PizzaModel();
+        pizzaModel.setIdPizzaModel(1);
+        pizzaModel.setPrecoPizzaModel(new BigDecimal("59.00"));
+        //pizzaModel.setListaIngredientesPizzaModel();
+        pizzaModel.setCategoriaPizzaModel(CategoriaPizza.LIGHT);
+
+        BebidaModel bebidaModel = new BebidaModel();
+        bebidaModel.setIdBebidaModel(1);
+        bebidaModel.setNomeBebidaModel("Sua mãe");
+        bebidaModel.setQuantidadeBebidaModel(17);
+        bebidaModel.setPrecoBebidaModel(new BigDecimal("59.00"));
+
+        listPizzaModel.add(pizzaModel);
+        listBebidaModel.add(bebidaModel);
+
+        IngredienteModel ingredienteModel = new IngredienteModel();
+        ingredienteModel.setIdIngredienteModel(1);
+        ingredienteModel.setNomeIngredienteModel("Calabresa");
+        ingredienteModel.setQuantidadeIngredienteModel(3);
+        ingredienteModel.setPrecoIngredienteModel(new BigDecimal(3.99));
+
+        listPedidoModel.add(pedidoModel);
+        listIngredienteModel.add(ingredienteModel);
         return cozinheiroModel;
     }
 
--- App-Pizzaria/src/test/java/com/system/application/pizzaria/viewModel/CozinheiroVMAdapterTest.java
+++ App-Pizzaria/src/test/java/com/system/application/pizzaria/viewModel/CozinheiroVMAdapterTest.java
@@ -1,15 +1,19 @@
 package com.system.application.pizzaria.viewModel;
 
-import com.system.application.pizzaria.entity.Cozinheiro;
-import com.system.application.pizzaria.entity.Ingrediente;
-import com.system.application.pizzaria.entity.Pedido;
+import com.system.application.pizzaria.entity.*;
+import com.system.application.pizzaria.entity.enums.CategoriaPizza;
+import com.system.application.pizzaria.entity.enums.StatusPedido;
 import com.system.application.pizzaria.exception.CozinheiroException;
 import com.system.application.pizzaria.viewmodel.CozinheiroVM;
-import com.system.application.pizzaria.viewmodel.IngredienteVM;
 import com.system.application.pizzaria.viewmodel.PedidoVM;
+import com.system.application.pizzaria.viewmodel.IngredienteVM;
+import com.system.application.pizzaria.viewmodel.PizzaVM;
+import com.system.application.pizzaria.viewmodel.BebidaVM;
 import com.system.application.pizzaria.viewmodel.adapter.CozinheiroVMAdapter;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -66,6 +70,45 @@ public class CozinheiroVMAdapterTest {
         Cozinheiro cozinheiro = new Cozinheiro();
         List<Pedido> listPedido = new ArrayList<>();
         List<Ingrediente> listIngrediente = new ArrayList<>();
+        cozinheiro.setIdCozinheiro(1);
+        cozinheiro.setListaPizzaPedidoCozinheiro(listPedido);
+        cozinheiro.setListaIngredientesPizzaCozinheiro(listIngrediente);
+
+        Pedido pedido = new Pedido();
+        List<Pizza> listPizza = new ArrayList<>();
+        List<Bebida> listBebida = new ArrayList<>();
+        pedido.setIdPedido(1);
+        pedido.setStatusPedido(StatusPedido.AGUARDANDO);
+        pedido.setListaPizzaPedido(listPizza);
+        pedido.setListaBebidaPedido(listBebida);
+        pedido.setHorarioPedido(LocalDateTime.now());
+        pedido.setHorarioEstimadoPedido(LocalDateTime.now());
+        pedido.setPrecoPedido(new BigDecimal("59.00"));
+        pedido.setComentarioPedido("fnfsjngdsnofgjlig, dhufsufhsfgbhsbgf fdhiuashfihasidf.");
+
+        Pizza pizza = new Pizza();
+        pizza.setIdPizza(1);
+        pizza.setPrecoPizza(new BigDecimal("59.00"));
+        //pizza.setListaIngredientesPizza();
+        pizza.setCategoriaPizza(CategoriaPizza.LIGHT);
+
+        Bebida bebida = new Bebida();
+        bebida.setIdBebida(1);
+        bebida.setNomeBebida("Sua mãe");
+        bebida.setQuantidadeBebida(17);
+        bebida.setPrecoBebida(new BigDecimal("59.00"));
+
+        listPizza.add(pizza);
+        listBebida.add(bebida);
+
+        Ingrediente ingrediente = new Ingrediente();
+        ingrediente.setIdIngrediente(1);
+        ingrediente.setNomeIngrediente("Calabresa");
+        ingrediente.setQuantidadeIngrediente(3);
+        ingrediente.setPrecoIngrediente(new BigDecimal(3.99));
+
+        listPedido.add(pedido);
+        listIngrediente.add(ingrediente);
 
         return cozinheiro;
     }
@@ -74,6 +117,45 @@ public class CozinheiroVMAdapterTest {
         CozinheiroVM cozinheiroVM = new CozinheiroVM();
         List<PedidoVM> listPedidoVM = new ArrayList<>();
         List<IngredienteVM> listIngredienteVM = new ArrayList<>();
+        cozinheiroVM.setIdCozinheiroVM(1);
+        cozinheiroVM.setListaPizzaPedidoVMCozinheiroVM(listPedidoVM);
+        cozinheiroVM.setListaIngredientesPizzaVMCozinheiroVM(listIngredienteVM);
+
+        PedidoVM pedidoVM = new PedidoVM();
+        List<PizzaVM> listPizzaVM = new ArrayList<>();
+        List<BebidaVM> listBebidaVM = new ArrayList<>();
+        pedidoVM.setIdPedidoVM(1);
+        pedidoVM.setStatusPedidoVM(StatusPedido.AGUARDANDO);
+        pedidoVM.setListaPizzaVMPedidoVM(listPizzaVM);
+        pedidoVM.setListaBebidaVMPedidoVM(listBebidaVM);
+        pedidoVM.setHorarioPedidoVM(LocalDateTime.now());
+        pedidoVM.setHorarioEstimadoPedidoVM(LocalDateTime.now());
+        pedidoVM.setPrecoPedidoVM(new BigDecimal("59.00"));
+        pedidoVM.setComentarioPedidoVM("fnfsjngdsnofgjlig, dhufsufhsfgbhsbgf fdhiuashfihasidf.");
+
+        PizzaVM pizzaVM = new PizzaVM();
+        pizzaVM.setIdPizzaVM(1);
+        pizzaVM.setPrecoPizzaVM(new BigDecimal("59.00"));
+        //pizzaVM.setListaIngredientesPizzaVM();
+        pizzaVM.setCategoriaPizzaVM(CategoriaPizza.LIGHT);
+
+        BebidaVM bebidaVM = new BebidaVM();
+        bebidaVM.setIdBebidaVM(1);
+        bebidaVM.setNomeBebidaVM("Sua mãe");
+        bebidaVM.setQuantidadeBebidaVM(17);
+        bebidaVM.setPrecoBebidaVM(new BigDecimal("59.00"));
+
+        listPizzaVM.add(pizzaVM);
+        listBebidaVM.add(bebidaVM);
+
+        IngredienteVM ingredienteVM = new IngredienteVM();
+        ingredienteVM.setIdIngredienteVM(1);
+        ingredienteVM.setNomeIngredienteVM("Calabresa");
+        ingredienteVM.setQuantidadeIngredienteVM(3);
+        ingredienteVM.setPrecoIngredienteVM(new BigDecimal(3.99));
+
+        listPedidoVM.add(pedidoVM);
+        listIngredienteVM.add(ingredienteVM);
 
         return cozinheiroVM;
     }
