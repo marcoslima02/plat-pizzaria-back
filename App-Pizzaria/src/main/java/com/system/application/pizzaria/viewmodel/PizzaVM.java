f1ef38d74d516b0ec954b933e7da7d51c00af0bd
marcoslima02 <marcos.lima02@gmail.com>
2021-10-22 21:19:49 -0300

    feat(pizza): fixing errors in pizza adapters and tests
    Foi arrumado os adapter e os testes de pizza
    
    refs: PP-36

--- App-Pizzaria/src/main/java/com/system/application/pizzaria/viewmodel/PizzaVM.java
+++ App-Pizzaria/src/main/java/com/system/application/pizzaria/viewmodel/PizzaVM.java
@@ -1,20 +1,35 @@
 package com.system.application.pizzaria.viewmodel;
 
+import com.system.application.pizzaria.entity.Pizza;
 import com.system.application.pizzaria.entity.enums.CategoriaPizza;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.Hibernate;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
-@EqualsAndHashCode
 public class PizzaVM {
 
     private Integer idPizzaVM;
     private BigDecimal precoPizzaVM;
     private List<IngredienteVM> listaIngredientesPizzaVM;
     private CategoriaPizza categoriaPizzaVM;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        PizzaVM that = (PizzaVM) o;
+        return Objects.equals(idPizzaVM, that.idPizzaVM);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 }
